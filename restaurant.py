3a4b06abe9c66462797a2a563fc4e4268af359b1
gabyruth8 <146225286+gabyruth8@users.noreply.github.com>
2024-02-08 20:44:42 -0500

    Restaurant

---
+++ restaurant.py
@@ -0,0 +1,63 @@
+#CS175
+#Gabriella Velasquez
+#restaurant
+
+vegetarian = input("Is anyone in your party vegetarian? ").lower()
+vegan = input("Is anyone in your party vegan? ").lower()
+gluten_free = input("Is anyone in your party gluten free? ").lower()
+
+print("Here are your restaurant choices:")
+
+if vegetarian == "no":
+    if vegan == "no":
+        if gluten_free == "no":
+            print("Main Street Pizza Company")
+            print("Corner Café")
+            print("Mama's Fine Italian")
+            print("The Chef's Kitchen")
+
+if vegetarian == "yes":
+    if vegan == "no":
+        if gluten_free == "no":
+            print("Main Street Pizza Company")
+            print("Corner Café")
+            print("Mama's Fine Italian")
+            print("The Chef's Kitchen")
+
+if vegetarian == "no":
+    if vegan == "no":
+        if gluten_free == "yes":
+            print("Main Street Pizza Company")
+            print("Corner Café")
+            print("The Chef's Kitchen")
+
+if vegetarian == "yes":
+    if vegan == "no":
+        if gluten_free == "yes":
+            print("Main Street Pizza Company")
+            print("Corner Café")
+            print("The Chef's Kitchen")
+
+if vegetarian == "no":
+    if vegan == "yes":
+        if gluten_free == "no":
+            print("Corner Café")
+            print("The Chef's Kitchen")
+
+if vegetarian == "no":
+    if vegan == "yes":
+        if gluten_free == "yes":
+            print("Corner Café")
+            print("The Chef's Kitchen")
+          
+if vegetarian == "yes":
+    if vegan == "yes":
+        if gluten_free == "no":
+            print("Corner Café")
+            print("The Chef's Kitchen")
+
+if vegetarian == "yes":
+    if vegan == "yes":
+        if gluten_free == "yes":
+            print("Corner Café")
+            print("The Chef's Kitchen")
