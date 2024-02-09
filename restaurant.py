289439b93691fe933cdf637a8846ea352f845c70
gabyruth8 <146225286+gabyruth8@users.noreply.github.com>
2024-02-08 20:52:06 -0500

    Restaurant

--- restaurant.py
+++ restaurant.py
@@ -2,62 +2,72 @@
 #Gabriella Velasquez
 #restaurant
 
-vegetarian = input("Is anyone in your party vegetarian? ").lower()
-vegan = input("Is anyone in your party vegan? ").lower()
-gluten_free = input("Is anyone in your party gluten free? ").lower()
+while True:
+    vegetarian = input("Is anyone in your party vegetarian? ").lower()
+    vegan = input("Is anyone in your party vegan? ").lower()
+    gluten_free = input("Is anyone in your party gluten-free? ").lower()
 
-print("Here are your restaurant choices:")
+    print("\nHere are your restaurant choices:")
 
-if vegetarian == "no":
-    if vegan == "no":
-        if gluten_free == "no":
-            print("Main Street Pizza Company")
-            print("Corner Café")
-            print("Mama's Fine Italian")
-            print("The Chef's Kitchen")
+    if vegetarian == "no":
+        if vegan == "no":
+            if gluten_free == "no":
+                print("Main Street Pizza Company")
+                print("Corner Café")
+                print("Mama's Fine Italian")
+                print("The Chef's Kitchen")
 
-if vegetarian == "yes":
-    if vegan == "no":
-        if gluten_free == "no":
-            print("Main Street Pizza Company")
-            print("Corner Café")
-            print("Mama's Fine Italian")
-            print("The Chef's Kitchen")
+    if vegetarian == "yes":
+        if vegan == "no":
+            if gluten_free == "no":
+                print("Main Street Pizza Company")
+                print("Corner Café")
+                print("Mama's Fine Italian")
+                print("The Chef's Kitchen")
 
-if vegetarian == "no":
-    if vegan == "no":
-        if gluten_free == "yes":
-            print("Main Street Pizza Company")
-            print("Corner Café")
-            print("The Chef's Kitchen")
+    if vegetarian == "no":
+        if vegan == "no":
+            if gluten_free == "yes":
+                print("Main Street Pizza Company")
+                print("Corner Café")
+                print("The Chef's Kitchen")
 
-if vegetarian == "yes":
-    if vegan == "no":
-        if gluten_free == "yes":
-            print("Main Street Pizza Company")
-            print("Corner Café")
-            print("The Chef's Kitchen")
+    if vegetarian == "yes":
+        if vegan == "no":
+            if gluten_free == "yes":
+                print("Main Street Pizza Company")
+                print("Corner Café")
+                print("The Chef's Kitchen")
 
-if vegetarian == "no":
-    if vegan == "yes":
-        if gluten_free == "no":
-            print("Corner Café")
-            print("The Chef's Kitchen")
+    if vegetarian == "no":
+        if vegan == "yes":
+            if gluten_free == "no":
+                print("Corner Café")
+                print("The Chef's Kitchen")
 
-if vegetarian == "no":
-    if vegan == "yes":
-        if gluten_free == "yes":
-            print("Corner Café")
-            print("The Chef's Kitchen")
-          
-if vegetarian == "yes":
-    if vegan == "yes":
-        if gluten_free == "no":
-            print("Corner Café")
-            print("The Chef's Kitchen")
+    if vegetarian == "no":
+        if vegan == "yes":
+            if gluten_free == "yes":
+                print("Corner Café")
+                print("The Chef's Kitchen")
 
-if vegetarian == "yes":
-    if vegan == "yes":
-        if gluten_free == "yes":
-            print("Corner Café")
-            print("The Chef's Kitchen")
+    if vegetarian == "yes":
+        if vegan == "yes":
+            if gluten_free == "no":
+                print("Corner Café")
+                print("The Chef's Kitchen")
+
+    if vegetarian == "yes":
+        if vegan == "yes":
+            if gluten_free == "yes":
+                print("Corner Café")
+                print("The Chef's Kitchen")
+
+    another.search = input("Enter 'yes' if you would like to do another restaurant search (enter 'no' to end): ").lower()
+    
+    if another.search != 'yes':
+        break
+    
+                 
+
+
